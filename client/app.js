bea1e8b413542d9082a07788efd64f909a010c6d
Jinyan <jinyan.shen21@gmail.com>
2019-01-06 09:34:06 -0500

    feat(searchbox): in progress

--- client/app.js
+++ client/app.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import SearchBox from './components/SearchBox'
+import Books from './components/Books'
 
-const App = () => {
-  return (
-    <div>
-      <h1>Customized Boilermaker</h1>
-    </div>
-  )
+class App extends Component {
+  constructor () {
+    super()
+  }
+
+  render() {
+    const { books } = this.props
+    return (
+      <div>
+        <h1>SEARCH YOUR FAVORIATE BOOK:</h1>
+        <SearchBox />
+        { books.length && 
+          <Books books={books}/>
+        }
+      </div>
+    )
+  }
 }
 
-export default App
+//parameter state comes from client/index.js provider store state
+const mapState = state => {
+  console.log("STATE", state)
+  return {
+    books:state.books.books,
+    isPending: state.books.isPending
+  }
+}
+
+//dispatch the DOM changes to call an action, the function returns an object then uses connect to change the data from reducer
+const mapDispatch = dispatch => {
+  return {
+    onRequestBooks: (searchSelect, searchInput) => dispatch(requestBooks(searchSelect, searchInput))
+  }
+}
+
+export default connect(mapState, mapDispatch)(App)
+
