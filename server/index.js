fa802e9076aa09d368650b5848aacffdc0e49169
Jinyan <jinyan.shen21@gmail.com>
2019-01-05 23:56:17 -0500

    initiate project with basic setup

--- server/index.js
+++ server/index.js
@@ -5,13 +5,6 @@ const PORT = process.env.PORT || 8080
 const app = express()
 module.exports = app
 
-/**
- * In development environment, I keep all of the
- * app's secret API keys in a file called `secrets.js`, in the project
- * root. This file is included in the .gitignore - it will NOT be tracked
- * or show up on Github. 
- */
-if (process.env.NODE_ENV !== 'production') require('../secrets')
 
 const createApp = () => {
    // logging middleware
