96ebf93f438f6f29c02001cc472f60405cd91d8b
Jinyan <jinyan.shen21@gmail.com>
2019-01-10 13:54:14 -0500

    code-cleanup in progress

--- client/components/Books.js
+++ client/components/Books.js
@@ -5,15 +5,18 @@ import Book from './Book'
 const Books = ({ books, error, pickBook }) => {
     return (
         <div className='mt4'>
-            {error? 
-                <h3 className='center'>Oooops...No results can be found...</h3> :
-                <Card.Group className='center'>
+            {
+                //It is a 304 on the server if no results are found, so I make validation check 
+                //when response is returned from axios request and pass down error message as props 
+                //to Books component in order to render the error message when there is no search results.
+                error
+                ? <h3 className='center'>Oooops...No results can be found...</h3> 
+                : <Card.Group className='center'>
                     {
                         books.filter(book=> book.author_name).map((book, i) =>
-                            <Book book={book} pickBook={pickBook} key={i} />
-                        )
+                            <Book book={book} pickBook={pickBook} key={i} />)
                     }
-                </Card.Group>
+                  </Card.Group>
             }
         </div>
     )
